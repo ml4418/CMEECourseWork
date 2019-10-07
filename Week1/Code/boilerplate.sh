7d94b77dba0cc5860aff5258b9f80751f3ab9bcb
Mengemeng <m.liu18@imperial.ac.uk>
2019-10-07 16:15:06 +0100

    FIrst commit

---
+++ Week1/Code/boilerplate.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# Author: Mengmeng m.liu18@imperial.ac.uk
+# Script: boilerplate.sh
+# Desc: simple boilerplate for shell scripts
+# Arguments: none
+# Date: Oct 2019
+
+echo -e "\nThis is a shell script! \n"
+
+#exit
